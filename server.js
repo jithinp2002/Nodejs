cdbe86094db5e983c5ea77793dadeeec537908d8
Jithin Puthalath <jithinp2002@yahoo.com>
2012-12-10 01:42:25 -0500

    Need to fix the node server js
    using the wrong reuired statement

---
+++ server.js
@@ -0,0 +1,30 @@
+var sys = require("sys"),
+    ws =  require("net");
+
+var clients = [];
+
+ws.createServer(
+	function (websocket) {
+
+		clients.push(websocket);
+
+		websocket.addListener("connect", function (resource) {
+			// emitted after handshake
+			sys.debug("connect: " + resource);
+			websocket.write("jithin");
+		}).addListener("data", function (data) {
+			sys.debug("hi" + data);
+		// handle incoming data
+		// send data to ALL clients whenever ANY client send up data
+		for (var i = 0 ; i < clients.length ; i ++ ) {
+			clients[i].write(data);
+		}
+
+    }).addListener("close", function () {
+
+		// emitted when server or client closes connection
+		sys.debug("close");
+    });
+  }).listen(8080);
+
+  sys.debug("Listening on port 8080");
